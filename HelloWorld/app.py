4846c516efa2d710438acc98ba4d6200c79f246f
jayeshgurbani2525 <jayeshgurbani2525@gmail.com>
2021-06-28 17:43:55 +0530

    python files

---
+++ HelloWorld/app.py
@@ -0,0 +1,64 @@
+# print("jayesh gurbani")
+# print("0----")
+# print(" ||||")
+# print('*' * 10)
+# price = 10
+# print(price)
+# name = input('what is your name? ')
+# favorite_color = input('what is your favorite color')
+# print(name + 'likes' + favorite_color)
+# birth_year = input ('birth year: ')
+# print(type(birth_year))
+# age = 2021 - int(birth_year)
+# print(type(age))
+# course = '''
+# hi chad,
+
+# he is our first email to you.
+
+# Thank you,
+# The support team
+
+# '''
+# print(course)
+# course = 'python for beginners'
+# another = course[:]
+
+# print(another)
+# first = 'john'
+# last = 'smith'
+# message = first + ' [' + last + ' ] is a coder '
+# print (message)
+# course = ' python for beginners '
+# print(course.upper())
+# print(course.lower()) /
+# course = 'Python for beginners'
+# print('Python' in course )
+# print (10 % 3)
+# x = 10 
+# x = x + 3 
+# print (x)
+# x = 10 
+# x -= 3
+# print (x)
+# x = 10 + 3 * 2 
+# print (x)
+# x = (2 + 3) * 10 -3 
+# print (x) 
+# has_high_income = True 
+# has_good_credit = True
+# if has_high_income and has_good_credit:
+#    print ("Eligible for loan")
+# i = 1
+# while i<=5:
+# 	print (i)
+# 	i = i + 1
+# print ("done")
+
+temp = ''
+for item in 'python':
+	temp = item + '.' + temp + '.' + item
+
+print(temp)
+
+
---
+++ some.py
@@ -0,0 +1,2 @@
+print("Hello World")
+print("jayesh gurbani")
